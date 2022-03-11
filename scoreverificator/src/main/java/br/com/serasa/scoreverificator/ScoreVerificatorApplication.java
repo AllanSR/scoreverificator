9d935c4d28d76b661b58a6d902bea5ff7f0bd97a
Rodrigues <allan.da.s.rodrigues@accenture.com>
2022-03-11 09:51:03 -0300

    Criacao do projeto

---
+++ scoreverificator/src/main/java/br/com/serasa/scoreverificator/ScoreVerificatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.serasa.scoreverificator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreVerificatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreVerificatorApplication.class, args);
+	}
+
+}
---
+++ scoreverificator/src/test/java/br/com/serasa/scoreverificator/ScoreverificatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serasa.scoreverificator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreverificatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
